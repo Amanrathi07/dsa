c514d9a00156944c975638303b7fe8f977413931
Amanrathi07 <amanrathi4321@gmail.com>
2025-12-20 00:03:52 +0530

    print

---
+++ day1/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	//print
+	std::cout<<"hello world"; //std 
+	cout<<"hello world" ;  // using namespace
+
+	
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
