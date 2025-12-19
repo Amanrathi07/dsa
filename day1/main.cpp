debd77923eda9e882b4291f8a2869362bfa19609
Amanrathi07 <amanrathi4321@gmail.com>
2025-12-20 01:40:17 +0530

    uranary

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,139 +1,102 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-
-
-int main(){
+int main()
+{
 
 	// //print
 
-	// std::cout<<"hello "; //std 
+	// std::cout<<"hello "; //std
 	// cout<<"world , " ;  // using namespace
 
- 
 	// // line break (to go to next line )
 
 	// cout<<"i am using cpp" << endl ; //method 1
 
-	// cout <<" \n"; //method 
-
+	// cout <<" \n"; //method
 
 	// //same line
 	// cout<<"aman "<<"kumer "<<"rathi \n";
 
+	// //    variables
+	// //   int  , char , float , bool , double
 
+	// 	char fullname ='a' ;  // a is changed to its askai value then to binary  and stored in memory by
+	// 	int age = 25 ;  //integer
+	// 	float price = 5.99f ;// for desimal value and f is important
+	// 	bool  isTrue = true ; // true to 1 and false to 0
+	// 	double numb = 1000.99 ;
 
+	// 	cout<<fullname<<endl;
+	// 	cout<<age<<endl;
 
-// //    variables
-// //   int  , char , float , bool , double  
-	
-// 	char fullname ='a' ;  // a is changed to its askai value then to binary  and stored in memory by 
-// 	int age = 25 ;  //integer
-// 	float price = 5.99f ;// for desimal value and f is important 	
-// 	bool  isTrue = true ; // true to 1 and false to 0 
-// 	double numb = 1000.99 ;
-	
-// 	cout<<fullname<<endl;
-// 	cout<<age<<endl;
+	// // checkking the size of a variable
 
-// // checkking the size of a variable 
+	// 	cout<<"size of age in Byts : "<<sizeof(age)<<endl;
 
-// 	cout<<"size of age in Byts : "<<sizeof(age)<<endl;
-	
+	// type casting
+	//  impleset type conversi;on  : when we change data from small dt to large . ex int to float
+	//
+	//
+	//  explicit type casting : when user force a type change  mainly from large to small
 
+	// //implset typecasting
 
- // type casting  
- //  impleset type conversi;on  : when we change data from small dt to large . ex int to float 
- //
- //
- //  explicit type casting : when user force a type change  mainly from large to small
+	// 	char str = 'a';
 
-// //implset typecasting 
+	// 	int strValue = str ;
 
-// 	char str = 'a';
+	// 	cout<<strValue<<endl ;
 
-// 	int strValue = str ;
+	// // explicit
 
-// 	cout<<strValue<<endl ;
+	// 	double price = 1000.99 ;
 
-// // explicit
+	// 	int newPrice = (int)price ;
 
-// 	double price = 1000.99 ;
+	// 	cout<<newPrice <<endl ;
 
-// 	int newPrice = (int)price ;
+	// // inputs
 
-// 	cout<<newPrice <<endl ;
-	
+	//   int age ;  // currentaly have a garbage value
 
+	//   cout << "enter the age : ";
+	//   cin>> age ;
 
+	//   cout <<"you age is :" << age <<endl ;
 
+	// operators
 
+	// i   arithmetic
+	// ii  relationsl
+	// iii logical
 
+	// int a = 5, b = 6;
 
+	// int sum = a + b;
 
+	// cout << "sum is :" << sum << endl;
 
+	// cout << "sub id :" << (b - a) << endl;
 
-// // inputs
+	// cout << "type casting :" << (5 / (double)2) << endl;
 
+	// // relationsal (equal)
 
-//   int age ;  // currentaly have a garbage value 
+	// cout << "is 5 is equal to '5' :" << (5 == '5') << endl;
 
-//   cout << "enter the age : ";
-//   cin>> age ;
+	// logical (|| , && , !)
 
-//   cout <<"you age is :" << age <<endl ;
+	// uranary operator
 
-  
+	int a = 5;
 
-//operators 
+	int b = a++;
+	int c = ++a;
 
-// i   arithmetic 
-// ii  relationsl 
-// iii logical	
-
-    int a=5 ,  b=6 ;
-
-    int sum  = a+b ;
-
-    cout << "sum is :" << sum <<endl ;
-
-    cout <<"sub id :"<< (b-a) <<endl ;
-
-
-    cout<<"type casting :"<<(5/(double)2)<< endl;
-
-
-
-// relationsal (equal)
-
-
-    cout<<"is 5 is equal to '5' :"<<(5=='5')<<endl;
-
-
-// logical (|| , && , !) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	cout << "b= a++ :" << b << endl;
+	cout << "c= ++a :" << c << endl;
 
 	return 0;
 }
-
-
-
-
