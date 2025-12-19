9ee7471028e6a138d05758e538c2e4bb9f924372
Amanrathi07 <amanrathi4321@gmail.com>
2025-12-20 00:11:45 +0530

    simple print

--- day1/main.cpp
+++ day1/main.cpp
@@ -5,14 +5,20 @@ using namespace std;
 int main(){
 
 	//print
-	std::cout<<"hello world"; //std 
-	cout<<"hello world" ;  // using namespace
 
-	
+	// std::cout<<"hello world"; //std 
+	// cout<<"hello world" ;  // using namespace
+
+ 
+	// line break (to go to next line )
+
+	cout<<"hello world"<<endl; //method 1
+
+	cout <<"hello world \n"; //method 
 
 
-
-
+	//same line
+	cout<<"aman "<<"kumer "<<"rathi \n";
 
 
 
