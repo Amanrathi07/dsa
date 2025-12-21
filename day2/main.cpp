882752f49dc9acdf1589391fcc20e019bdef4e13
Amanrathi07 <amanrathi4321@gmail.com>
2025-12-21 11:28:58 +0530

    conitional

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,8 +1,18 @@
-#include<iosteam>
+#include<iostream>
 using namespace std ;
 
+// if-else
 int main(){
-  cout<<"working"<<endl ;
+
+  int n  ;
+  cout<<"enter your age : " ;
+  cin>>n ;
+
+  if(n >= 18){
+    cout<<"you can vote "<<endl ;
+  }else{
+    cout<<"you can't vote"<<endl ;
+  }
+
+  return 0;
 }
-
-
