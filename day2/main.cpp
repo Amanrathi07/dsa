81c915011b42505073ddd39ff935b7aed1070d3a
Amanrathi07 <amanrathi4321@gmail.com>
2025-12-21 11:33:53 +0530

    even odd

--- day2/main.cpp
+++ day2/main.cpp
@@ -4,15 +4,28 @@ using namespace std ;
 // if-else
 int main(){
 
-  int n  ;
-  cout<<"enter your age : " ;
+  // int n  ;
+  // cout<<"enter your age : " ;
+  // cin>>n ;
+
+  // if(n >= 18){
+  //   cout<<"you can vote "<<endl ;
+  // }else{
+  //   cout<<"you can't vote"<<endl ;
+  // }
+
+  // odd & even 
+
+  int n ;
+  cout<<"enter the value of n :"<<endl ;
   cin>>n ;
 
-  if(n >= 18){
-    cout<<"you can vote "<<endl ;
+  if (n%2==0){
+    cout<<n<<" is even number "<<endl;
   }else{
-    cout<<"you can't vote"<<endl ;
+    cout<<n<<" is odd number "<<endl ;
   }
+
 
   return 0;
 }
