6a029b02bec7c7aaed4d7a8091667bc6531bcb2c
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-01 01:19:05 +0530

    function

---
+++ day4/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std ;
+
+// function 
+
+void printHello(){
+  cout<<"hello world " << endl ;
+}
+
+
+int main(){
+ 
+  printHello() ;
+
+  return 0 ; 
+}
+
+
+
+
