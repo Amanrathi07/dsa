66b6020ffa38246a2b534071e69ac07f0424e27b
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-02 02:18:48 +0530

    dec to binary

--- day5/main.cpp
+++ day5/main.cpp
@@ -2,9 +2,22 @@
 using namespace std ;
 
 // binary number system 
+
+int decToBin(int num){
+    int ans = 0 ;
+    int pow =1 ;
+    while(0<num){
+        int rem = num%2 ;
+        num=num/2 ;
+        ans += rem*pow ;
+        pow = pow*10 ;
+    }
+    return ans ;
+}
+
 int main(){
-    cout<<"working"<<endl ;
-    
+
+    cout<<decToBin(6)<<endl;
 
 
     return 0 ;
