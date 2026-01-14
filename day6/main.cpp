d9ce20822b8cfcc655bbf2594d5c68ed11c2fc67
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-14 22:57:29 +0530

    left shift and right shift

--- day6/main.cpp
+++ day6/main.cpp
@@ -7,12 +7,21 @@ int main(){
     // bitwise operators 
     // & | ^ 
 
+// bitwise and
     cout<<(4 & 5)<<endl ;
     cout<<(4 & 8)<<endl ;
 
+// bitwise or
     cout<<(4 | 5)<<endl ;
     cout<<(4 | 8)<<endl ;
 
+//bitwise xor
+    cout<<(4^8)<<endl ;
+
+// bitwise leftshift  <<    and >> right shift 
+
+// n<<i 
+cout<<(6<<1)<<endl ;
 
     return 0 ;
 }
