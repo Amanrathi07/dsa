19eb908e59144c4b0642e99cbee8e1e0a13896b6
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-15 23:24:03 +0530

    simple q

--- day7/main.cpp
+++ day7/main.cpp
@@ -9,9 +9,27 @@ int main(){
     int marks[5];
     int newMarks[] ={1,2,3,4,5} ;
 
-    for(int i = 0 ;i<sizeof(newMarks)/sizeof(int) ;i++ ){
-        cout<<newMarks[i]<<endl ;
+    //print an array 
+
+    int size = sizeof(newMarks)/sizeof(int);
+    // for(int i = 0 ;i<size ;i++ ){
+    //     cout<<newMarks[i]<<endl ;
+    // }
+
+    // Q > find smallest and largest in array 
+
+    int num = newMarks[0] ;
+
+    for(int i = 0 ;i<size ;i++ ){
+        if(newMarks[i]<num){
+            num = newMarks[i] ;
+        }
+        // if(newMarks[i]>num){
+        //  num = newMarks[i] ;     //for the largest 
+        // }
     }
+
+    cout<<num <<endl ;
 
     return 0 ;
 }
