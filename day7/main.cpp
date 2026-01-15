7d8f06761f2da630ef363bc4e28da9161079fb94
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-15 23:16:35 +0530

    print array

--- day7/main.cpp
+++ day7/main.cpp
@@ -9,7 +9,9 @@ int main(){
     int marks[5];
     int newMarks[] ={1,2,3,4,5} ;
 
-    
+    for(int i = 0 ;i<sizeof(newMarks)/sizeof(int) ;i++ ){
+        cout<<newMarks[i]<<endl ;
+    }
 
     return 0 ;
 }
