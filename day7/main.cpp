3487fe6b84516e2d2c003e242f2c525f79298397
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-15 23:37:14 +0530

    pass by refrence

--- day7/main.cpp
+++ day7/main.cpp
@@ -2,45 +2,65 @@
 
 using namespace std ;
 
+
+void changeArr(int arr[],int size){
+    
+
+    for(int i = 0 ; i<size ; i++){
+        arr[i]=arr[i]*2 ;
+    }
+
+}
+
+
 int main(){
 // array , first data structure 
 
-    // creation 
-    int marks[5];
-    int newMarks[] ={1,2,3,4,5,43,5,763254,456,} ;
+//     // creation 
+//     int marks[5];
+//     int newMarks[] ={1,2,3,4,5,43,5,763254,456,} ;
 
-    //print an array 
+//     //print an array 
 
-    int size = sizeof(newMarks)/sizeof(int);
-    // for(int i = 0 ;i<size ;i++ ){
-    //     cout<<newMarks[i]<<endl ;
-    // }
+//     int size = sizeof(newMarks)/sizeof(int);
+//     // for(int i = 0 ;i<size ;i++ ){
+//     //     cout<<newMarks[i]<<endl ;
+//     // }
 
-    // Q > find smallest and largest in array 
+//     // Q > find smallest and largest in array 
 
-// ====================================================================
+// // ====================================================================
 
-    int num = newMarks[0] ;
-    int index ;
-    for(int i = 0 ;i<size ;i++ ){
+//     int num = newMarks[0] ;
+//     int index ;
+//     for(int i = 0 ;i<size ;i++ ){
 
-        // if(newMarks[i]<num){
-        //     num = newMarks[i] ;
-        //     index = i ;
-        // }
+//         // if(newMarks[i]<num){
+//         //     num = newMarks[i] ;
+//         //     index = i ;
+//         // }
         
-        if(newMarks[i]>num){
-         num = newMarks[i] ;     //for the largest 
-         index = i ;
-        }
-    }
+//         if(newMarks[i]>num){
+//          num = newMarks[i] ;     //for the largest 
+//          index = i ;
+//         }
+//     }
+
+    // cout<<num <<endl ;
+    // cout<<index <<endl ;
 
 // ====================================================================
 
+    // pass by value(primitive) and pass by refrence(non primitive) 
+    
+    int marks[] = {1,2,3,4,5} ;
+    int size = sizeof(marks)/sizeof(int);
+    changeArr(marks,size) ;
 
-
-    cout<<num <<endl ;
-    cout<<index <<endl ;
+    for(int i = 0 ; i<5 ; i++){
+        cout<<marks[i]<<endl ;
+    }
+   
 
     return 0 ;
 }
