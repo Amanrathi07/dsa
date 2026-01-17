659656d7a562c77531c324b6c14ad3696f3b4568
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-18 00:14:32 +0530

    vector pass by refrence

--- day8/main.cpp
+++ day8/main.cpp
@@ -2,29 +2,41 @@
 #include <vector>
 using namespace std;
 
+
+void reverseVector(vector<int> &nums){
+    int start = 0 ;
+    int end = nums.size()-1 ;
+    while(start<end){
+        swap(nums[start],nums[end]) ;
+        start ++ ;
+        end  -- ;
+    }
+        
+}
+
 int main()
 {
 
     // vectors : dynamic in size
     // stl : std template lib
-    cout << "vectors" << endl;
+    // cout << "vectors" << endl;
 
     // creation
 
-    vector<int> vec; // 0
-    vector<char> vec1 = {'a', 'b', 'c', 'd'};
+    // vector<int> vec; // 0
+    // vector<char> vec1 = {'a', 'b', 'c', 'd'};
     // vector<int>vec2(x,y) ;   //x is size of vector ,y is value at eact index
 
 //====================================================================
 
     // for each loop
 
-    for (char i : vec1)
-    { // i is value
-        cout << i << endl;
-    }
+    // for (char i : vec1)
+    // { // i is value
+    //     cout << i << endl;
+    // }
 
-    cout << "size = " << vec1.size() << endl;
+    // cout << "size = " << vec1.size() << endl;
 
 //======================================================================
 
@@ -40,18 +52,43 @@ int main()
 //====================================================================
     //front
 
-    cout<<"vector at front : "<<vec1.front()<<endl ;
+    // cout<<"vector at front : "<<vec1.front()<<endl ;
     
     //back
     
-    cout<<"vector at back : "<<vec1.back()<<endl ;
+    // cout<<"vector at back : "<<vec1.back()<<endl ;
 
 //====================================================================
 
     // at (index at num)
 
-    cout<<vec1.at(2)<<endl ;
+    // cout<<vec1.at(2)<<endl ;
 
+
+//====================================================================
+
+    // static vs dynamic allocation 
+
+    // arr.size()  & arr.capacity()
+
+//====================================================================
+
+    //Q single number 
+    vector< int> nums = {1,2,3,4} ;
+    //  int res =0 ; 
+    //     for(int value : nums){
+    //         res = res ^ value ;
+    //     }
+    //  cout<<res<<endl  ;
+
+//====================================================================
+
+    reverseVector(nums);
+
+    for(int i : nums){
+        cout<<i<<endl ;
+    }
 
     return 0;
 }
+
