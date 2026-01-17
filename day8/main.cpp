40a7bcb243e25a2b11850fef7ea5ea2832e8944d
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-17 23:26:12 +0530

    push.back

--- day8/main.cpp
+++ day8/main.cpp
@@ -1,27 +1,35 @@
-#include<iostream>
-#include<vector>
-using namespace std ;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int main(){
+int main()
+{
 
-    // vectors : dynamic in size 
+    // vectors : dynamic in size
     // stl : std template lib
-    cout<< "vectors"<<endl ;
+    cout << "vectors" << endl;
 
-    // creation 
+    // creation
 
-    vector<int>vec ; //0
-    vector<char>vec1 = {'a','b','c','d'} ;
-    // vector<int>vec2(x,y) ;   //x is size of vector ,y is value at eact index  
+    vector<int> vec; // 0
+    vector<char> vec1 = {'a', 'b', 'c', 'd'};
+    // vector<int>vec2(x,y) ;   //x is size of vector ,y is value at eact index
 
-//====================================================================
+    //====================================================================
 
-    // for each loop 
+    // for each loop
 
-    for(char i:vec1){    // i is value 
-        cout<<i<<endl ;
+    for (char i : vec1)
+    { // i is value
+        cout << i << endl;
     }
 
+    cout << "size = " << vec1.size() << endl;
 
-    return 0 ;
+    //====================================================================
+
+    vec1.push_back('e');
+    cout << "size after push back = " << vec1.size() << endl;
+
+    return 0;
 }
