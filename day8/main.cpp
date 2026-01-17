cdb56d6de1a656427bb2a520b8341a73a65762ea
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-17 23:18:03 +0530

    creation

--- day8/main.cpp
+++ day8/main.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
+#include<vector>
 using namespace std ;
 
 int main(){
 
-    cout<<"working"<<endl ;
+    // vectors : dynamic in size 
+    // stl : std template lib
+    cout<< "vectors"<<endl ;
+
+    // creation 
+
+    vector<int>vec ; //0
+    vector<int>vec1 = {1,2,3} ;
+    // vector<int>vec2(x,y) ;   //x is size of vector ,y is value at eact index  
+
+    cout<<vec1[0]<<endl ;
 
     return 0 ;
 }
