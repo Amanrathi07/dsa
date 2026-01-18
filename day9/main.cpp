1c5ec90a7415db333214259a773ea6156a4d3967
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-18 23:54:53 +0530

    simple

--- day9/main.cpp
+++ day9/main.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main(){
    
-    
+    for(int i = 0 ; i<5 ; i++){
+        for(int j = 4 ; j>i ; j--){
+            cout<<"*";
+        }
+        cout<<endl ;
+    }
+
     
     return 0;
 }
 
-
-// 
