81076c95e3661efb43af29c90eab6a385f5f6e23
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-18 23:55:24 +0530

    subarray

--- day9/main.cpp
+++ day9/main.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include<iostream>
+#include<vector>
+using namespace std ;
 
 int main(){
-   
-    for(int i = 0 ; i<5 ; i++){
-        for(int j = 4 ; j>i ; j--){
-            cout<<"*";
-        }
-        cout<<endl ;
-    }
+    // maximum array sum 
+    int n = 5 ;
+    vector<int>arr = {1,2,3,4,5} ;
 
-    
-    return 0;
+   // print all subarray of arr ;
+
+   for(int st = 0 ; st<n ; st++){
+    for(int en =st ; en<n ;en++ ){
+        for(int i ; i<n;i++){
+            cout<<arr[i] ;
+        }
+    }
+    cout<<endl;
+   }
+    return 0 ;
 }
 
+//                       st    en   time
+// 1 12 123 1234 12345   1      
+// 2 23 234 2345        2
+// 3 34 345               3
+// 4 45                     4
+// 5            
