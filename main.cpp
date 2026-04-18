92fd9cc4edd8bbf07bc9e2a56bc83b07356ba157
Amanrathi07 <89161729+Amanrathi07@users.noreply.github.com>
2026-04-18 19:49:33 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(){
  cout <<"pointer"<<endl;
  int num = 10 ;
 
- int* prt = num ;
+ int* prt = &num;  // & gives the address of num
  cout <<< prt << endl; // print num addres
 return 0;
 }
