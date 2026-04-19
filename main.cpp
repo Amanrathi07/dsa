feeaeb9eb838a5985339bb8a0388a41a91a456ec
Amanrathi07 <89161729+Amanrathi07@users.noreply.github.com>
2026-04-19 23:35:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
- cout <<"pointer"<<endl;
- int num = 10 ;
+     cout <<"pointer"<<endl;
+     int num = 10 ;
 
- int* prt = &num;  // & gives the address of num
- cout << prt << endl; // print num addres
+     int* prt = &num;  // & gives the address of num
+     cout << prt << endl; // print num addres
 
  // derefrensing 
 
- cout <<*prt << endl ; // print 10 
-return 0;
+     cout <<*prt << endl ; // print 10 
+    return 0;
 }
 
 
